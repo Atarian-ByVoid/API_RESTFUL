dc910a72ea3f9c20a97e368f13ad52fc71181cfa
carlosAlbertoGlobal <carlos.alberto@globalfinanceiro.com.br>
2023-03-14 17:50:39 -0300

    Save

--- src/main/java/com/planotatico/demo/exceptions/UnsupportedMathOperationException.java
+++ src/main/java/com/planotatico/demo/exceptions/UnsupportedMathOperationException.java
@@ -1,4 +1,4 @@
-package com.planotatico.demo;
+package com.planotatico.demo.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;

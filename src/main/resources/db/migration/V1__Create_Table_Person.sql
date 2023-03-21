ed5c23b4883ac9d668acabe10675ea2181920e84
carlosAlbertoGlobal <carlos.alberto@globalfinanceiro.com.br>
2023-03-21 12:36:00 -0300

    Migrations com flyway

--- src/main/resources/db/migration/V1__Create_Table_Person.sql
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -28,11 +28,11 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.person (
-    id bigint NOT NULL,
-    adress character varying(80) NOT NULL,
+    id bigint NOT NULL, 
     first_name character varying(80) NOT NULL,
-    gender character varying(6) NOT NULL,
-    last_name character varying(80) NOT NULL
+    last_name character varying(80) NOT NULL,
+    adress character varying(80) NOT NULL,
+    gender character varying(6) NOT NULL
 );
 
 

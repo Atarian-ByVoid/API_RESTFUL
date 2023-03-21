1b2ea6ff305aa860a6886844080ab21038ad4085
carlosAlbertoGlobal <carlos.alberto@globalfinanceiro.com.br>
2023-03-21 13:00:55 -0300

    Exclusão do serialization para continuidade

--- src/main/java/com/planotatico/demo/v1/PersonVO.java
+++ src/main/java/com/planotatico/demo/v1/PersonVO.java
@@ -2,19 +2,12 @@ package com.planotatico.demo.v1;
 
 import java.io.Serializable;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
  
-@JsonPropertyOrder({"id","address","first_name","last_name","gender"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private Long id;
-
-    @JsonProperty("first_name")
     private String firstName;
-
-    @JsonProperty("last_name")
     private String lastName;
     private String adress;
     private String gender;

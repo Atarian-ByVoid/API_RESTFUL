17c38d4b22879684fa44aecbd458802fc102070a
carlosAlbertoGlobal <carlos.alberto@globalfinanceiro.com.br>
2023-04-25 18:02:05 -0300

    feat: Incompleto

---
+++ src/main/java/com/global/demo/services/FileStorageService.java
@@ -0,0 +1,47 @@
+package com.global.demo.services;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.global.demo.config.FileStorageConfig;
+import com.global.demo.exceptions.FileStorageException;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig FileStorageConfig) {
+
+        Path path = Paths.get(FileStorageConfig.getUploadDir())
+                .toAbsolutePath().normalize();
+
+        this.fileStorageLocation = path;
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+
+        } catch (Exception e) {
+
+            throw new FileStorageException("Could not create the director where the uploaded files will be stored!", e);
+        }
+
+    }
+
+    public String storeFile(MultipartFile file){
+        String filename="";
+
+        try {
+            
+        } catch (Exception e) {
+            throw new FileStorageException("Could not be store file"+filename+".Ple",e);
+        }
+    }
+
+}
